0d85d2a8babb98e85c6a7783066e68d27443d8c7
Aleksei Milenko <amylenko@mail.ru>
2021-12-25 14:59:32 +0300

    Fixes

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -18,21 +19,19 @@ public class CardTest {
 
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue(DataGenerator.generateCity());
-        $("[data-test-id='date'] input").click();
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(DataGenerator.generateDate(3));
         $("[data-test-id='name'] input").setValue(DataGenerator.generateName());
         $("[data-test-id='phone'] input").setValue(DataGenerator.generatePhone());
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Запланировать")).click();
-        $(withText("Встреча успешно запланирована на")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
 
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(DataGenerator.generateDate(4));
+        $$("button").find(exactText("Запланировать")).click();
 
         $$("button").find(exactText("Перепланировать")).shouldBe(Condition.visible).click();
-
-//        $$("button").find(exactText("Перепланировать")).click();
-        
-        $(withText("Встреча упешно запланирована на")).shouldBe(Condition.visible);
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(4)));
     }
 }
